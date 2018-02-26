60a105bdc3ae1026234933dd050d527517246026
Dybdal, Aksel <aksel.dybdal@finn.no>
2018-02-26 10:42:05 +0100

    Initial Commit

---
+++ CaptainNavigator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CaptainNavigator
+//
+//  Created by Dybdal, Aksel on 26.02.2018.
+//  Copyright © 2018 Aksel Dybdal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
